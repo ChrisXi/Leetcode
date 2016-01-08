651786d7ef1705795453caca1b5d94c707bddb78
Xi Yang <clcblueljw@gmail.com>
2016-01-08 14:38:44 -0500

    #108 solved

---
+++ 108.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+    	
+    	int len = nums.length;
+
+    	return addChiild(nums, 0, len-1);
+    }
+
+    public TreeNode addChild(int[] nums, int s, int e) {
+
+    	if(s>e) {
+    		return null;
+    	}
+
+    	if(s == e) {
+    		return new TreeNode(nums[s]);
+    	}
+
+    	int index = (e+s)/2;
+    	TreeNode node = new TreeNode(nums[index]);
+
+    	node.left = addChild(nums, s, index-1);
+    	node.right = addChild(nums, index+1, e);
+
+    	return node;
+    }
+}
+
+
