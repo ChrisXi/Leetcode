cec1a70c144b5d7fc7a58439a4508bcf032ecb77
Xi Yang <clcblueljw@gmail.com>
2016-01-14 23:26:26 -0500

    #155 solved

---
+++ 155.java
@@ -0,0 +1,20 @@
+class MinStack {
+
+	Stack<Integer> s = new Stack<Integer>();
+
+    public void push(int x) {
+        
+    }
+
+    public void pop() {
+        
+    }
+
+    public int top() {
+        
+    }
+
+    public int getMin() {
+        
+    }
+}
