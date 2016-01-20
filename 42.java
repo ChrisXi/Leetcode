4a975495a07623df66775c988026294014d0004c
Xi Yang <clcblueljw@gmail.com>
2016-01-20 01:14:03 -0500

    #42 solved

---
+++ 42.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int trap(int[] height) {
+        
+        int len = height.length;
+
+        int[] leftMax = new int[len];
+        int[] rightMax = new int[len];
+
+        
+        int max = 0;
+        for(int i=0; i<len; i++) {
+        	leftMax[i] = max;
+        	max = Math.max(height[i], max);
+        }
+
+        max = 0;
+        for(int i=len-1; i>=0; i--) {
+        	rightMax[i] = max;
+        	max = Math.max(height[i], max);
+        }
+
+        int water = 0;
+        for(int i=0; i<len; i++) {
+        	if(height[i]<rightMax[i] && height[i]<leftMax[i]) {
+
+        	}
+        }
+
+
+    }
+}
