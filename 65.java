244551af3c51c6430456b6a952f4463737307641
Xi Yang <clcblueljw@gmail.com>
2016-01-21 23:57:58 -0500

    #65 solved

---
+++ 65.java
@@ -0,0 +1,68 @@
+public class Solution {
+    public boolean isNumber(String s) {
+        
+        int len = s.length();
+
+        boolean findNumber = false;
+        boolean findSpace = false;
+        boolean findDot = false;
+        boolean findE = false;
+
+        for(int i=0; i<len; i++) {
+        	if('0' <= s.charAt(i) && s.charAt(i) <= '9') {
+        		findNumber = true;
+        		if(findSpace) {
+        			return false;
+        		}
+        	} else if(s.charAt(i) == ' ') {
+        		if(findNumber || findDot) {
+        			findSpace = true;
+        		}
+        	} else if(s.charAt(i) == '+' || s.charAt(i) == '-') {
+
+        		if(findNumber || findSpace) {
+        			return false;
+        		} 
+
+    			if(i+1>=len || '9' < s.charAt(i+1) || s.charAt(i+1) < '0') {
+					return false;
+    			}
+        	} else if(s.charAt(i) == '.') {
+
+        		if(findSpace || findE || findDot)
+        			return false;
+
+        		findDot = true;
+
+    			if(i+1<len && (('9' < s.charAt(i+1) || s.charAt(i+1) < '0') && s.charAt(i+1) != 'e' && s.charAt(i+1) != ' ')) {
+					return false;
+    			} else if(i+1<len && s.charAt(i+1) == ' ') {
+    				if(i-1<0 || '9' < s.charAt(i-1) || s.charAt(i-1) < '0') {
+						return false;
+    				}
+    			}
+
+        	} else if(s.charAt(i) == 'e') {
+        		if(findSpace || findE)
+        			return false;
+
+        		findE = true;
+
+        		if(findNumber) {
+					if(i+1>=len || '9' < s.charAt(i+1) || s.charAt(i+1) < '0') {
+						return false;
+        			}
+        		} else {
+        			return false;
+        		}
+        	} else {
+        		return false;
+        	}
+        }
+
+        if(!findNumber)
+        	return false;
+
+        return true;
+    }
+}
